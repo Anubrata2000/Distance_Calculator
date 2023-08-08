a1647e2018975f325bef33c55cbf3d0b9d2a58e5
Anubrata2000 <109304431+Anubrata2000@users.noreply.github.com>
2023-08-08 21:33:16 +0530

    Updated commit

--- app/src/main/java/com/example/distance_calc/MainActivity.java
+++ app/src/main/java/com/example/distance_calc/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     TextView a;
@@ -16,19 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-        public void Dist_calc() {
+        public void Dist_calc(View view) {
         x1 = (EditText) findViewById(R.id.X1);
         x2 = (EditText) findViewById(R.id.X2);
         y1 = (EditText) findViewById(R.id.Y1);
         y2 = (EditText) findViewById(R.id.Y2);
-        a = (TextView) findViewById(R.id.Result);
+        a = (TextView) findViewById(R.id.result);
         double a1 = Double.parseDouble(x1.getText().toString());
         double a2 = Double.parseDouble(x2.getText().toString());
         double b1 = Double.parseDouble(y1.getText().toString());
         double b2 = Double.parseDouble(y2.getText().toString());
         double r = (a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1);
         double res = Math.sqrt(r);
-        a.setText("result="+String.valueOf(res));
+        a.setText(String.valueOf(res));
 
         }
 }
